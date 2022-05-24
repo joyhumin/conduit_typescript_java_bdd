99f31717ed97d75f8f27fb68482da96e6daf8ea8
Joy HU <joyhumin@gmail.com>
2022-05-24 19:57:49 +1200

    feat: add api module and open api spec

---
+++ conduit-api/src/main/java/nz/co/joyhu/Generated.java
@@ -0,0 +1,12 @@
+package nz.co.joyhu;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(CONSTRUCTOR)
+public @interface Generated {
+}
